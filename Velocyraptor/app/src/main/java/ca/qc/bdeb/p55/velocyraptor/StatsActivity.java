4625a8546eb4db0623fb40eb336c8741aef653a4
gohugo <hug472@hotmail.com>
2015-12-04 14:27:18 -0800

    jai enlever laction bar de statActivity et je lai remplacer par une toolBar Maintenant il faut touver une facon de fair comme les tab view avec une toolbar.
    les toolbar ne suportent pas les tab, mais il existe slinding  PagerSlidingTabStrip;
    
    http://stackoverflow.com/questions/27534211/how-to-create-toolbar-tabs-with-viewpager-in-material-design

--- Velocyraptor/app/src/main/java/ca/qc/bdeb/p55/velocyraptor/StatsActivity.java
+++ Velocyraptor/app/src/main/java/ca/qc/bdeb/p55/velocyraptor/StatsActivity.java
@@ -19,7 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class StatsActivity extends AppCompatActivity implements ActionBar.TabListener {
+public class StatsActivity extends AppCompatActivity{
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -62,25 +62,15 @@ public class StatsActivity extends AppCompatActivity implements ActionBar.TabLis
         // tab. We can also use ActionBar.Tab#select() to do this if we have
         // a reference to the Tab.
 
-//        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                actionBar.setSelectedNavigationItem(position);
-//            }
-//        });
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
 
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
 
-//            actionBar.addTab(
-//                    actionBar.newTab()
-//                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-//                            .setTabListener(this));
-        }
+            }
+        });
+
+
     }
 
 
@@ -98,20 +88,7 @@ public class StatsActivity extends AppCompatActivity implements ActionBar.TabLis
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
+   
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
